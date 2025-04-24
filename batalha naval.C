dd2e15ed1fc0acd4fede25f9d74c220b9e2ad11f
uVini-dev <vs28072005@gmail.com>
2025-04-24 19:10:17 +0000

    adição dos navios na diagonal

--- batalha naval.C
+++ batalha naval.C
@@ -30,6 +30,15 @@ int main(){
     int colunaVertical = 5;  // 6ª coluna
 
 
+             // Posiçoes iniciais em que os navios diagonais irão ocupar
+    
+    int linhaDiagonal1 = 6;  // 7ª linha
+    int colunaDiagonal1 = 3;  // 4ª coluna
+
+    int linhaDiagonal2 = 0; // 1ª linha
+    int colunaDiagonal2 = 9; // 10ª linha
+
+
              //  Navio horizontal
 
     int navioHorizontal[tamanhoNavio] = {3, 3, 3};  // Array representando o navio horizontal, [tamanho do navio] {numero represntando o navio}
@@ -69,6 +78,45 @@ int main(){
         if (!sobreposicao) { 
             for (int j = 0; j < tamanhoNavio; j++) {   // estrutura de repetição para repetir 3 linhas na mesma coluna a paritr da posição inicial
                 tabuleiro[linhaVertical + j][colunaVertical] = navioVertical[j];       
+            }
+        }
+    }
+     
+
+                // Navio Diagonal 1
+
+    int navioDiagonal1[tamanhoNavio] = {3, 3, 3};   // Array representando o navio diagonal 1, [tamanho do navio] {numero represntando o navio}
+    
+    if (linhaDiagonal1 + tamanhoNavio <= linhas && colunaDiagonal1 + tamanhoNavio <= colunas) {      //  detecção se esta dentro do tabuleiro
+        int sobreposicao = 0;
+        for (int i = 0; i < tamanhoNavio; i++) {
+            if (tabuleiro[linhaDiagonal1 + i][colunaDiagonal1 + i] != 0) {    // verificção se ja tem um navio na posição
+                sobreposicao = 1;       // detecção de algo diferente de agua
+                break;      // em caso de colisão, algo diferente de 0, o loop é parado
+            }
+        }
+        if (!sobreposicao) {
+            for (int i = 0; i < tamanhoNavio; i++) {      // estrutura de repetição para sobrepor na diagonal a partir da posição inicial
+                tabuleiro[linhaDiagonal1 + i][colunaDiagonal1 + i] = navioDiagonal1[i];
+            }
+        }
+    }   
+
+                    // Navio Diagonal 2     
+
+    int navioDiagonal2[tamanhoNavio] = {3, 3, 3};     // Array representando o navio diagonal 2, [tamanho do navio] {numero represntando o navio}
+    
+    if (linhaDiagonal2 + tamanhoNavio <= linhas && colunaDiagonal2 - tamanhoNavio >= 0) {   //  detecção se esta dentro do tabuleiro
+        int sobreposicao = 0;
+        for (int i = 0; i < tamanhoNavio; i++) {
+            if (tabuleiro[linhaDiagonal2 + i][colunaDiagonal2 - i] != 0) {       // verificção se ja tem um navio na posição
+                sobreposicao = 1;     // detecção de algo diferente de agua
+                break;        // em caso de colisão, algo diferente de 0, o loop é parado
+            }
+        }
+        if (!sobreposicao) {
+            for (int i = 0; i < tamanhoNavio; i++) {          // estrutura de repetição para sobrepor na diagonal a partir da posição inicial
+                tabuleiro[linhaDiagonal2 + i][colunaDiagonal2 - i] = navioDiagonal2[i];
             }
         }
     }
